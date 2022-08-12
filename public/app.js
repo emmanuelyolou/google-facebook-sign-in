00e0e26cba3880fff9e8813ee68e1d53f86d55d3
Emmanuel Yolou <emmanuelyolou@gmail.com>
2022-08-12 10:56:51 +0000

    google button has the same width as the input fields

--- public/app.js
+++ public/app.js
@@ -14,6 +14,7 @@ function initialize() {
         document.querySelector('#google-sign-in'),
         { 
             theme: "outline", size: "large",
+            width: "272px" //calculated to fit the same size as inputs
         }
     );
 }
@@ -41,3 +42,33 @@ function parseJwt (token) {
 
     return JSON.parse(jsonPayload);
 };
+
+
+//Facebook
+const fbAppId = "773896347069718";
+window.fbAsyncInit = function() {
+    FB.init({
+      appId      : fbAppId,
+      cookie     : true,
+      xfbml      : true,
+      version    : 'v14.0'
+    });
+      
+    FB.AppEvents.logPageView();   
+
+    FB.getLoginStatus(function(response) {
+        console.log(response);
+    });
+      
+  };
+
+  //Initializes the javascript sdk
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "https://connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+
+ 
